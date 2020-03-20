dcccba13c056f155bc04b4b2caede92881f78ea8
Michal Budzyn <michalbudzyn@gmail.com>
2020-03-20 20:48:37 +0100

    response header versions for DescribeClientQuotas and AlterClientQuotas

--- proxy/protocol/request_key_version.go
+++ proxy/protocol/request_key_version.go
@@ -28,217 +28,235 @@ func (r *RequestKeyVersion) decode(pd packetDecoder) (err error) {
 // See also public short responseHeaderVersion(short _version) in kafka/clients/src/generated/java/org/apache/kafka/common/message/ApiMessageType.java
 func (r *RequestKeyVersion) ResponseHeaderVersion() int16 {
 	switch r.ApiKey {
-	case 0:
+	case 0: // Produce
 		return 0
-	case 1:
+	case 1: // Fetch
 		return 0
-	case 2:
+	case 2: // ListOffset
 		return 0
-	case 3:
+	case 3: // Metadata
 		if r.ApiVersion >= 9 {
 			return 1
 		} else {
 			return 0
 		}
-	case 4:
+	case 4: // LeaderAndIsr
 		if r.ApiVersion >= 4 {
 			return 1
 		} else {
 			return 0
 		}
-	case 5:
+	case 5: // StopReplica
 		if r.ApiVersion >= 2 {
 			return 1
 		} else {
 			return 0
 		}
-	case 6:
+	case 6: // UpdateMetadata
 		if r.ApiVersion >= 6 {
 			return 1
 		} else {
 			return 0
 		}
-	case 7:
+	case 7: // ControlledShutdown
 		if r.ApiVersion >= 3 {
 			return 1
 		} else {
 			return 0
 		}
-	case 8:
+	case 8: // OffsetCommit
 		if r.ApiVersion >= 8 {
 			return 1
 		} else {
 			return 0
 		}
-	case 9:
+	case 9: // OffsetFetch
 		if r.ApiVersion >= 6 {
 			return 1
 		} else {
 			return 0
 		}
-	case 10:
+	case 10: // FindCoordinator
 		if r.ApiVersion >= 3 {
 			return 1
 		} else {
 			return 0
 		}
-	case 11:
+	case 11: // JoinGroup
 		if r.ApiVersion >= 6 {
 			return 1
 		} else {
 			return 0
 		}
-	case 12:
+	case 12: // Heartbeat
 		if r.ApiVersion >= 4 {
 			return 1
 		} else {
 			return 0
 		}
-	case 13:
+	case 13: // LeaveGroup
 		if r.ApiVersion >= 4 {
 			return 1
 		} else {
 			return 0
 		}
-	case 14:
+	case 14: // SyncGroup
 		if r.ApiVersion >= 4 {
 			return 1
 		} else {
 			return 0
 		}
-	case 15:
+	case 15: // DescribeGroups
 		if r.ApiVersion >= 5 {
 			return 1
 		} else {
 			return 0
 		}
-	case 16:
+	case 16: // ListGroups
 		if r.ApiVersion >= 3 {
 			return 1
 		} else {
 			return 0
 		}
-	case 17:
-		if r.ApiVersion >= 2 {
-			return 1
-		} else {
-			return 0
-		}
-	case 18:
+	case 17: // SaslHandshake
 		return 0
-	case 19:
+	case 18: // ApiVersions
+		// ApiVersionsResponse always includes a v0 header.
+		// See KIP-511 for details.
+		return 0
+	case 19: // CreateTopics
 		if r.ApiVersion >= 5 {
 			return 1
 		} else {
 			return 0
 		}
-	case 20:
+	case 20: // DeleteTopics
 		if r.ApiVersion >= 4 {
 			return 1
 		} else {
 			return 0
 		}
-	case 21:
-		return 0
-	case 22:
+	case 21: // DeleteRecords
 		if r.ApiVersion >= 2 {
 			return 1
 		} else {
 			return 0
 		}
-	case 23:
+	case 22: // InitProducerId
+		if r.ApiVersion >= 2 {
+			return 1
+		} else {
+			return 0
+		}
+	case 23: // OffsetForLeaderEpoch
 		return 0
-	case 24:
+	case 24: // AddPartitionsToTxn
 		return 0
-	case 25:
+	case 25: // AddOffsetsToTxn
 		return 0
-	case 26:
+	case 26: // EndTxn
 		return 0
-	case 27:
+	case 27: // WriteTxnMarkers
 		return 0
-	case 28:
+	case 28: // TxnOffsetCommit
 		if r.ApiVersion >= 3 {
 			return 1
 		} else {
 			return 0
 		}
-	case 29:
+	case 29: // DescribeAcls
 		if r.ApiVersion >= 2 {
 			return 1
 		} else {
 			return 0
 		}
-	case 30:
+	case 30: // CreateAcls
+		if r.ApiVersion >= 2 {
+			return 1
+		} else {
+			return 0
+		}
+	case 31: // DeleteAcls
+		if r.ApiVersion >= 2 {
+			return 1
+		} else {
+			return 0
+		}
+	case 32: // DescribeConfigs
 		return 0
-	case 31:
+	case 33: // AlterConfigs
 		return 0
-	case 32:
+	case 34: // AlterReplicaLogDirs
 		return 0
-	case 33:
-		return 0
-	case 34:
-		return 0
-	case 35:
-		return 0
-	case 36:
+	case 35: // DescribeLogDirs
 		if r.ApiVersion >= 2 {
 			return 1
 		} else {
 			return 0
 		}
-	case 37:
+	case 36: // SaslAuthenticate
 		if r.ApiVersion >= 2 {
 			return 1
 		} else {
 			return 0
 		}
-	case 38:
+	case 37: // CreatePartitions
 		if r.ApiVersion >= 2 {
 			return 1
 		} else {
 			return 0
 		}
-	case 39:
+	case 38: // CreateDelegationToken
 		if r.ApiVersion >= 2 {
 			return 1
 		} else {
 			return 0
 		}
-	case 40:
+	case 39: // RenewDelegationToken
 		if r.ApiVersion >= 2 {
 			return 1
 		} else {
 			return 0
 		}
-	case 41:
+	case 40: // ExpireDelegationToken
 		if r.ApiVersion >= 2 {
 			return 1
 		} else {
 			return 0
 		}
-	case 42:
+	case 41: // DescribeDelegationToken
 		if r.ApiVersion >= 2 {
 			return 1
 		} else {
 			return 0
 		}
-	case 43:
+	case 42: // DeleteGroups
 		if r.ApiVersion >= 2 {
 			return 1
 		} else {
 			return 0
 		}
-	case 44:
+	case 43: // ElectLeaders
+		if r.ApiVersion >= 2 {
+			return 1
+		} else {
+			return 0
+		}
+	case 44: // IncrementalAlterConfigs
 		if r.ApiVersion >= 1 {
 			return 1
 		} else {
 			return 0
 		}
-	case 45:
+	case 45: // AlterPartitionReassignments
 		return 1
-	case 46:
+	case 46: // ListPartitionReassignments
 		return 1
-	case 47:
+	case 47: // OffsetDelete
+		return 0
+	case 48: // DescribeClientQuotas
+		return 0
+	case 49: // AlterClientQuotas
 		return 0
 	default:
 		// throw new UnsupportedVersionException("Unsupported API key " + apiKey);
